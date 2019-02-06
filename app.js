6de83361ce24e4a983150a98e2b35ee211ae7266
RavinduW <raviduchinthaka3@gmail.com>
2019-02-06 09:16:29 +0530

    db configuration setup with mongodb

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
 const express = require('express'); //use express
 const expressLayouts = require('express-ejs-layouts');
+const mongoose = require('mongoose'); //use the Mongoose ODM
 
 const app = express(); 
+
+//db configuration
+const db = require('./config/keys').MongoURI;
+
+//connect to mongo
+mongoose.connect(db,{ userNewUrlParser:true})
+    .then(() => console.log('Connected to mongodb successfully!'))
+    .catch(err => console.log(err));
 
 //use embedded js as the template engine
 app.use(expressLayouts);
 app.set('view engine','ejs');
+
+//bodyparser middleware
+app.use(express.urlencoded({extended:false}));
 
 //routes
 app.use('/',require('./routes/index')); //index
